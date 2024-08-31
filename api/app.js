6fd1246e57393129e7eb5aa9274777e03ad8900a
martinondiwa <surekondiwa@gmail.com>
2024-08-31 12:45:54 +0300

    set up

--- api/app.js
+++ api/app.js
@@ -1,7 +1,22 @@
-import e from "express";
+import express from "express";
 
-const app = e();
+const app = express();
 
+app.use("/api/test", (req, res) => {
+    res.send("It works!")
+})
+app.use("/api/auth/register", (req, res) => {
+    res.send("It works!")
+})
+app.use("/api/auth/login", (req, res) => {
+    res.send("It works!")
+})
+app.use("/api/auth/logout", (req, res) => {
+    res.send("It works!")
+})
+app.use("/api/auth/logout", (req, res) => {
+    res.send("It works!")
+})
 app.listen(5500, () => {
-    console.log("Server is running!");
+    console.log("server is running!"); 'server is running!'
 });
