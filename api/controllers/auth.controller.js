4606b3132e493d99f555d60fc44f1eda9c3a3360
Martin Ondiwa <96140915+martinondiwa@users.noreply.github.com>
2026-03-16 21:24:33 -0500

    Fix  register function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 export const register = (req,res)=>{
-    //db operations annotation
+    //db operations annotations
     console.log ("register endpoint") 'register endpoint'
 }
 export const login = (req,res)=>{
